85737f8abe8300956903e090cf8ee2ded743a6d0
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2023-02-02 16:09:39 +0500

    init: copy project and create 3 models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistModule } from './models/artist/artist.module';
+import { TrackModule } from './models/track/track.module';
+import { UserModule } from './models/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ArtistModule,
+    TrackModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
