c0e9b334f30dccd8598579ad97033e3bb44bedc1
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2023-02-06 22:47:04 +0500

    feat: add env file get port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 // import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
   // const config = new DocumentBuilder()
   //   .setTitle('NODEJS2022Q4-service')
   //   .setDescription('Home music library service')
@@ -14,6 +16,6 @@ async function bootstrap() {
   // const document = SwaggerModule.createDocument(app, config);
   // SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
--- src/models/users/users.module.ts
+++ src/models/users/users.module.ts
@@ -2,20 +2,9 @@ import { UsersMiddleware } from './users.middleware';
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import * as Joi from 'joi';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'test', 'provision')
-          .default('development'),
-        PORT: Joi.number().default(4000),
-      }),
-    }),
-  ],
+  imports: [],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
