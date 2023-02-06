a6588d0665389d8ef4070f3974f352ec3199a518
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2023-02-06 23:43:07 +0500

    docs: change readme

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as yaml from 'js-yaml';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const PORT = configService.get<number>('PORT');
+  const PORT = configService.get<number>('PORT') || 4000;
 
   const document: any = yaml.load(
     await readFile('./doc/api.yaml', { encoding: 'utf-8' }),
