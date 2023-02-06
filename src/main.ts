f005b7d2051fa2bb694aa437dcf9de61134184cb
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2023-02-06 23:31:42 +0500

    feat: add swagger route

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { readFile } from 'node:fs/promises';
+import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT');
-  // const config = new DocumentBuilder()
-  //   .setTitle('NODEJS2022Q4-service')
-  //   .setDescription('Home music library service')
-  //   .setVersion('1.0')
-  //   .addTag('Users')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+
+  const document: any = yaml.load(
+    await readFile('./doc/api.yaml', { encoding: 'utf-8' }),
+  );
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(PORT);
 }
