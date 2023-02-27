27df6e19273b6aaec83b4084090efd470cf2020d
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2023-02-27 18:54:29 +0500

    fix: delete writing with timestamp for loging

--- src/models/logger/logger.ts
+++ src/models/logger/logger.ts
@@ -1,4 +1,4 @@
-import { existsSync, mkdirSync, statSync, writeFileSync } from 'fs';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { MyLogger } from './myloger.service';
 
@@ -11,27 +11,16 @@ export function logger(message:any, context:any, type:string, stack:string = '')
             }
             const newLog = `${type}${message} - ${stack}'\n'`;
             let filePath = join(dir, MyLogger.lastLog.toString());
-            const { size } = statSync(filePath);
-
-            if (size > +process.env.LOGGER_SIZE) {
-                MyLogger.lastLog = Date.now();
-                filePath = join('./logs', MyLogger.lastLog.toString());
-            }
             writeFileSync(filePath+'.txt', newLog, {flag: 'as'})
+
         } else {
             const dir = join('./', 'logs');
             if (!existsSync('./'+dir)) {
                 mkdirSync(dir);
             }
-
             const newLog = `${type}${context} - ${message}'\n'`;
             let filePath = join('./logs', MyLogger.lastLog.toString());
-            const { size } = statSync(filePath);
 
-            if (size > +process.env.LOGGER_SIZE) {
-                MyLogger.lastLog = Date.now();
-                filePath = join('./logs', MyLogger.lastLog.toString());
-            }
             writeFileSync(filePath+'.txt', newLog, {flag: 'as'})
         }
     } catch (error) {
